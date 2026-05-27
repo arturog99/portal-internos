6adf2c8938dbd8547b1c38f3edab42812a8e2a82
Arturo <arex12345678910@gmail.com>
2026-05-27 16:19:31 +0200

    feat: crear barra de navegación responsive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProjectListComponent } from './components/project-list/project-list';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ProjectListComponent],
+  imports: [RouterOutlet, ProjectListComponent, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
