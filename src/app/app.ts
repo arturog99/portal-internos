a8097449a89c90230e5c44ad2a235bbed6037ee3
Arturo <arex12345678910@gmail.com>
2026-05-27 13:33:18 +0200

    feat: integrar ProjectListComponent en app principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProjectListComponent } from './components/project-list/project-list';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProjectListComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
