b9821a16247754c443ca4930deb0b251ea07ee7f
Arturo <arex12345678910@gmail.com>
2026-06-01 12:04:19 +0200

    feat: implementar Angular Router con páginas de Tecnologías y Documentación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { ProjectListComponent } from './components/project-list/project-list';
+import { Technologies } from './components/technologies/technologies';
+import { Documentation } from './components/documentation/documentation';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/proyectos', pathMatch: 'full' },
+  { path: 'proyectos', component: ProjectListComponent },
+  { path: 'tecnologias', component: Technologies },
+  { path: 'documentacion', component: Documentation },
+  { path: '**', redirectTo: '/proyectos' }
+];
